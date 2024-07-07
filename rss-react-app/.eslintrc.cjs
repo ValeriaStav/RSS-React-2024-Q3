caa719d00a83e6ca13c0e393cbce63e20cfee5c8
ValeriaStav <ronnyramb@gmail.com>
2024-07-07 14:30:05 +0300

    feat: integrate Prettier for code formatting

--- rss-react-app/.eslintrc.cjs
+++ rss-react-app/.eslintrc.cjs
@@ -2,23 +2,28 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react-refresh", "react-compiler"],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-refresh', 'react-compiler'],
   rules: {
-    "@typescript-eslint/no-explicit-any": "error",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true }
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    "react-compiler/react-compiler": "error"
+    'react-compiler/react-compiler': 'error',
+    'prettier/prettier': 'error',
   },
   settings: {
     react: {
-      version: "detect"
-    }
-  }
+      version: 'detect',
+    },
+  },
 };
