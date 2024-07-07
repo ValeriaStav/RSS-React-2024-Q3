9079601081807f9c0b349dad981277c5119ec04a
ValeriaStav <ronnyramb@gmail.com>
2024-07-07 18:27:10 +0300

    feat: add SearchBar component

---
+++ rss-react-app/src/components/SearchBar.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component<{ onSearch: (searchInput: string) => void }> {
+  state = {
+    searchInput: '',
+  };
+
+  componentDidMount() {
+    const savedSearchInput = localStorage.getItem('searchInput');
+    if (savedSearchInput) {
+      this.setState({ searchInput: savedSearchInput }, () => {
+        this.props.onSearch(savedSearchInput);
+      });
+    }
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchInput = event.target.value;
+    this.setState({ searchInput });
+    localStorage.setItem('searchInput', searchInput);
+  };
+
+  handleSearch = () => {
+    const { searchInput } = this.state;
+    const trimmedSearchInput = searchInput.trim();
+    this.props.onSearch(trimmedSearchInput);
+    localStorage.setItem('searchInput', trimmedSearchInput);
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          name="searchInput"
+          value={this.state.searchInput}
+          onChange={this.handleChange}
+          placeholder="Enter search query"
+        />
+        <button onClick={this.handleSearch}>Search</button>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
