fff963bac0db9795c41c570c7428b4662461a27b
ValeriaStav <ronnyramb@gmail.com>
2024-07-12 21:13:28 +0300

    refactor: convert SearchResults component to functional component

--- rss-react-app/src/components/SearchResults.tsx
+++ rss-react-app/src/components/SearchResults.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { Character } from '../services/api';
 import '../css/SearchResults.css';
 
@@ -6,28 +5,24 @@ interface SearchResultsProps {
   results: Character[];
 }
 
-class SearchResults extends Component<SearchResultsProps> {
-  render() {
-    const { results } = this.props;
-
-    return (
-      <div className="SearchResults">
-        {results.length === 0 ? (
-          <p>No results found.</p>
-        ) : (
-          <div className="cardsContainer">
-            {results.map((result, index) => (
-              <div className="card" key={`${result.name}-${index}`}>
-                <h3>{result.name}</h3>
-                <p>height: {result.height}</p>
-                <p>mass: {result.mass}</p>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+const SearchResults = ({ results }: SearchResultsProps) => {
+  return (
+    <div className="SearchResults">
+      {results.length === 0 ? (
+        <p>No results found.</p>
+      ) : (
+        <div className="cardsContainer">
+          {results.map((result, index) => (
+            <div className="card" key={`${result.name}-${index}`}>
+              <h3>{result.name}</h3>
+              <p>height: {result.height}</p>
+              <p>mass: {result.mass}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default SearchResults;
