d2564a1043af619fb2d14d5364906ec2d6b63c1b
ValeriaStav <ronnyramb@gmail.com>
2024-07-13 00:32:26 +0300

    feat: setup and configure Vitest and React Testing Library

--- rss-react-app/vite.config.ts
+++ rss-react-app/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,4 +6,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/RSS-React-2024-Q3/rss-react-app/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text', 'html'],
+    },
+  },
 });
---
+++ rss-react-app/vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      include: ['src/**/*.tsx'],
+      exclude: ['node_modules', 'test'],
+      all: true,
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
+        },
+      },
+    },
+  },
+});
