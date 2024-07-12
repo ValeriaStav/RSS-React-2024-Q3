bb95271cccac11c3d290eba127be6ebebccbcb57
ValeriaStav <ronnyramb@gmail.com>
2024-07-12 22:19:05 +0300

    feat: add a 404 page

---
+++ rss-react-app/src/components/404.tsx
@@ -0,0 +1,12 @@
+import '../css/404.css';
+
+const NotFound = () => {
+  return (
+    <div className="NotFound">
+      <p className="title">404</p>
+      <p className="text">Page Not Found</p>
+    </div>
+  );
+};
+
+export default NotFound;
--- rss-react-app/src/routes/AppRoutes.tsx
+++ rss-react-app/src/routes/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from '../App';
+import NotFound from '../components/404';
 
 const AppRoutes = () => {
   return (
     <Router basename="/RSS-React-2024-Q3/rss-react-app">
       <Routes>
         <Route path="/" element={<App />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
