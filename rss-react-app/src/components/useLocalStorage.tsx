ea68976fb87eebd6f728a3ed9d62823582996bd0
ValeriaStav <ronnyramb@gmail.com>
2024-07-16 15:22:12 +0300

    feat: custom hook for search query LS handling

---
+++ rss-react-app/src/components/useLocalStorage.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const useLocalStorage = (key: string, initialValue: string) => {
+  const [storedValue, setStoredValue] = useState<string>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? item : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: string) => {
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, value);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { storedValue, setValue };
+};
+
+export default useLocalStorage;
