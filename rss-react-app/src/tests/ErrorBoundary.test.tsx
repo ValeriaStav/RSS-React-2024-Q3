da4fb8fdf5c6e812a260e0f99d54971c5409dae6
ValeriaStav <ronnyramb@gmail.com>
2024-07-23 12:49:58 +0300

    test: add tests for ErrorBoundary

---
+++ rss-react-app/src/tests/ErrorBoundary.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ErrorBoundary from '../components/ErrorBoundary';
+
+describe('ErrorBoundary component', () => {
+  test('renders child component when there is no error', () => {
+    const Child = () => <div>Child component</div>;
+
+    render(
+      <ErrorBoundary>
+        <Child />
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText('Child component')).toBeInTheDocument();
+  });
+
+  test('catches error and displays fallback UI', () => {
+    const ErrorComponent = () => {
+      throw new Error('Test error');
+    };
+
+    render(
+      <ErrorBoundary>
+        <ErrorComponent />
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText('Something went wrong😔')).toBeInTheDocument();
+    expect(
+      screen.getByText('We apologize for the inconvenience')
+    ).toBeInTheDocument();
+  });
+
+  test('resets error state when "Try again" button is clicked', () => {
+    const ErrorComponent = () => {
+      throw new Error('Test error');
+    };
+    const Child = () => <div>Child component</div>;
+
+    const { rerender } = render(
+      <ErrorBoundary>
+        <ErrorComponent />
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText('Something went wrong😔')).toBeInTheDocument();
+
+    rerender(
+      <ErrorBoundary>
+        <Child />
+      </ErrorBoundary>
+    );
+
+    fireEvent.click(screen.getByText('Try again'));
+
+    expect(
+      screen.queryByText('Something went wrong😔')
+    ).not.toBeInTheDocument();
+    expect(screen.getByText('Child component')).toBeInTheDocument();
+  });
+});
